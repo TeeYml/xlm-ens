cb3e2f8d2e3a3d46f7919c3d8f9fe8ea3d8e9315
ObaHacker <dynateolabs@gmail.com>
2026-04-23 02:40:02 +0100

    Fixed issue #87

--- packages/xlm-ns-common/src/lib.rs
+++ packages/xlm-ns-common/src/lib.rs
@@ -7,3 +7,4 @@ pub mod validation;
 pub use constants::*;
 pub use errors::CommonError;
 pub use types::{NameHash, NameRecord, Tld};
+pub use validation::{parse_fqdn, validate_chain_name, validate_label, validate_owner, validate_registration_years};
--- packages/xlm-ns-common/src/validation.rs
+++ packages/xlm-ns-common/src/validation.rs
@@ -67,3 +67,98 @@ pub fn validate_chain_name(chain: &str) -> Result<(), CommonError> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        parse_fqdn, validate_chain_name, validate_label, validate_registration_years,
+    };
+    use crate::constants::{
+        MAX_NAME_LENGTH, MAX_REGISTRATION_YEARS, MIN_NAME_LENGTH, MIN_REGISTRATION_YEARS,
+    };
+    use crate::errors::CommonError;
+    use crate::types::Tld;
+
+    #[test]
+    fn rejects_short_labels() {
+        let short_label = "a".repeat(MIN_NAME_LENGTH - 1);
+
+        assert_eq!(
+            validate_label(&short_label),
+            Err(CommonError::NameTooShort)
+        );
+    }
+
+    #[test]
+    fn rejects_long_labels() {
+        let long_label = "a".repeat(MAX_NAME_LENGTH + 1);
+
+        assert_eq!(validate_label(&long_label), Err(CommonError::NameTooLong));
+    }
+
+    #[test]
+    fn rejects_unsupported_tlds() {
+        assert_eq!(
+            parse_fqdn("valid.eth"),
+            Err(CommonError::UnsupportedTld)
+        );
+    }
+
+    #[test]
+    fn rejects_invalid_characters() {
+        assert_eq!(
+            validate_label("ab_"),
+            Err(CommonError::InvalidCharacters)
+        );
+        assert_eq!(
+            parse_fqdn("ab_.xlm"),
+            Err(CommonError::InvalidCharacters)
+        );
+    }
+
+    #[test]
+    fn rejects_labels_with_invalid_hyphen_boundaries() {
+        assert_eq!(
+            validate_label("-abc"),
+            Err(CommonError::InvalidLabelBoundary)
+        );
+        assert_eq!(
+            validate_label("abc-"),
+            Err(CommonError::InvalidLabelBoundary)
+        );
+    }
+
+    #[test]
+    fn accepts_valid_fqdn() {
+        assert_eq!(parse_fqdn("abc-123.xlm"), Ok(("abc-123".to_string(), Tld::Xlm)));
+    }
+
+    #[test]
+    fn enforces_registration_year_bounds() {
+        assert_eq!(
+            validate_registration_years(MIN_REGISTRATION_YEARS),
+            Ok(())
+        );
+        assert_eq!(
+            validate_registration_years(MAX_REGISTRATION_YEARS),
+            Ok(())
+        );
+        assert_eq!(
+            validate_registration_years(MIN_REGISTRATION_YEARS - 1),
+            Err(CommonError::InvalidRegistrationPeriod)
+        );
+        assert_eq!(
+            validate_registration_years(MAX_REGISTRATION_YEARS + 1),
+            Err(CommonError::InvalidRegistrationPeriod)
+        );
+    }
+
+    #[test]
+    fn validates_chain_name_presence() {
+        assert_eq!(validate_chain_name("stellar"), Ok(()));
+        assert_eq!(
+            validate_chain_name("   "),
+            Err(CommonError::EmptyChainName)
+        );
+    }
+}
